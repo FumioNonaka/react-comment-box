6f496aaede7981619658d86a50ecaae3c3b31a92
fumio nonaka <fumio.nonaka@staff.gu3.co.jp>
2019-06-12 12:33:50 +0900

    React入門 04: JSONデータをローカルファイルから読み込んでページに表示する

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import CommentList from './components/CommentList';
 import CommentForm from './components/CommentForm';
 import './App.css';
 
-function App() {
-  const data = [
-    {author: "ヘンリー・キッシンジャー", text: "チャンスは__貯金__できない。"},
-    {author: "マーク・トウェイン", text: "禁煙なんてたやすい。私は*何千回*もやった。"}
-  ];
-  return (
-    <div className="App">
-      <h1>コメント</h1>
-      <CommentList data={data} />
-      <CommentForm />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+  }
+  async componentDidMount() {
+    const url = 'comments.json';
+    try {
+      const jsonData = 
+        await (
+          await fetch(url)
+        ).json();
+      this.setState({data: jsonData});
+    } catch (error) {
+      console.error('エラー:', error);
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>コメント</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
 }
 
 export default App;
