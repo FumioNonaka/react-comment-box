c51c0657224522f6a0b731cf80e278b16bb12b5b
fumio nonaka <fumio.nonaka@staff.gu3.co.jp>
2019-06-13 10:40:49 +0900

    React入門 05: フォームからページにデータを追加する

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,11 +1,43 @@
 import React from 'react';
 
-const CommentForm = () => {
-  return (
-    <div className="CommentForm">
-      CommentFormを表示
-    </div>
-  );
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {author: '', text: ''};
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleAuthorChange(event) {
+    this.setState({author: event.target.value});
+  }
+  handleTextChange(event) {
+    this.setState({text: event.target.value});
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onCommentSubmit({author: this.state.author, text: this.state.text});
+    this.setState({author: '', text: ''});
+  }
+  render() {
+    return (
+      <form className="CommentForm"  onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="名前"
+            value={this.state.author}
+            onChange={this.handleAuthorChange}
+          />
+          <input
+            type="text"
+            placeholder="コメントを入力"
+            value={this.state.text}
+            onChange={this.handleTextChange}
+          />
+          <input type="submit" value="送信" />
+        </form>
+    );
+  }
 };
 
 export default CommentForm;
